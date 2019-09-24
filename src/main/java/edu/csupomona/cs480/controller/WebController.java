565d8207d9d738ef2b57ab1be099ce499fc60c77
John Escalona <johnrichmondescalona@gmail.com>
2019-09-24 10:15:33 -0700

    Added mapping for Assingment 3

--- src/main/java/edu/csupomona/cs480/controller/WebController.java
+++ src/main/java/edu/csupomona/cs480/controller/WebController.java
@@ -14,12 +14,11 @@ import edu.csupomona.cs480.App;
 import edu.csupomona.cs480.data.User;
 import edu.csupomona.cs480.data.provider.UserManager;
 
-
 /**
  * This is the controller used by Spring framework.
  * <p>
- * The basic function of this controller is to map
- * each HTTP API Path to the correspondent method.
+ * The basic function of this controller is to map each HTTP API Path to the
+ * correspondent method.
  *
  */
 
@@ -27,22 +26,18 @@ import edu.csupomona.cs480.data.provider.UserManager;
 public class WebController {
 
 	/**
-	 * When the class instance is annotated with
-	 * {@link Autowired}, it will be looking for the actual
-	 * instance from the defined beans.
+	 * When the class instance is annotated with {@link Autowired}, it will be
+	 * looking for the actual instance from the defined beans.
 	 * <p>
-	 * In our project, all the beans are defined in
-	 * the {@link App} class.
+	 * In our project, all the beans are defined in the {@link App} class.
 	 */
 	@Autowired
 	private UserManager userManager;
 
 	/**
-	 * This is a simple example of how the HTTP API works.
-	 * It returns a String "OK" in the HTTP response.
-	 * To try it, run the web application locally,
-	 * in your web browser, type the link:
-	 * 	http://localhost:8080/cs480/ping
+	 * This is a simple example of how the HTTP API works. It returns a String "OK"
+	 * in the HTTP response. To try it, run the web application locally, in your web
+	 * browser, type the link: http://localhost:8080/cs480/ping
 	 */
 	@RequestMapping(value = "/cs480/ping", method = RequestMethod.GET)
 	String healthCheck() {
@@ -53,14 +48,13 @@ public class WebController {
 	}
 
 	/**
-	 * This is a simple example of how to use a data manager
-	 * to retrieve the data and return it as an HTTP response.
+	 * This is a simple example of how to use a data manager to retrieve the data
+	 * and return it as an HTTP response.
 	 * <p>
-	 * Note, when it returns from the Spring, it will be
-	 * automatically converted to JSON format.
+	 * Note, when it returns from the Spring, it will be automatically converted to
+	 * JSON format.
 	 * <p>
-	 * Try it in your web browser:
-	 * 	http://localhost:8080/cs480/user/user101
+	 * Try it in your web browser: http://localhost:8080/cs480/user/user101
 	 */
 	@RequestMapping(value = "/cs480/user/{userId}", method = RequestMethod.GET)
 	User getUser(@PathVariable("userId") String userId) {
@@ -69,17 +63,14 @@ public class WebController {
 	}
 
 	/**
-	 * This is an example of sending an HTTP POST request to
-	 * update a user's information (or create the user if not
-	 * exists before).
+	 * This is an example of sending an HTTP POST request to update a user's
+	 * information (or create the user if not exists before).
 	 *
 	 * You can test this with a HTTP client by sending
-	 *  http://localhost:8080/cs480/user/user101
-	 *  	name=John major=CS
+	 * http://localhost:8080/cs480/user/user101 name=John major=CS
 	 *
-	 * Note, the URL will not work directly in browser, because
-	 * it is not a GET request. You need to use a tool such as
-	 * curl.
+	 * Note, the URL will not work directly in browser, because it is not a GET
+	 * request. You need to use a tool such as curl.
 	 *
 	 * @param id
 	 * @param name
@@ -87,9 +78,7 @@ public class WebController {
 	 * @return
 	 */
 	@RequestMapping(value = "/cs480/user/{userId}", method = RequestMethod.POST)
-	User updateUser(
-			@PathVariable("userId") String id,
-			@RequestParam("name") String name,
+	User updateUser(@PathVariable("userId") String id, @RequestParam("name") String name,
 			@RequestParam(value = "major", required = false) String major) {
 		User user = new User();
 		user.setId(id);
@@ -98,30 +87,30 @@ public class WebController {
 		userManager.updateUser(user);
 		return user;
 	}
+
 	/**
 	 * This API deletes the user. It uses HTTP DELETE method.
 	 *
 	 * @param userId
 	 */
 	@RequestMapping(value = "/cs480/user/{userId}", method = RequestMethod.DELETE)
-	void deleteUser(
-			@PathVariable("userId") String userId) {
+	void deleteUser(@PathVariable("userId") String userId) {
 		userManager.deleteUser(userId);
 	}
-	
+
 	/**
 	*
 	*/
 	@RequestMapping(value = "/cs480/user/something", method = RequestMethod.GET)
-	String printSomething(){
+	String printSomething() {
 		return "Hello this is our group project";
 	}
-	
+
 	/**
 	*
 	*/
 	@RequestMapping(value = "/cs480/user/mihir", method = RequestMethod.GET)
-	String printSomething(){
+	String printSomething() {
 		return "Greetings From Mihir! ";
 	}
 
@@ -129,10 +118,15 @@ public class WebController {
 	*
 	*/
 	@RequestMapping(value = "/cs480/user/whatever", method = RequestMethod.GET)
-	String printSomething(){
+	String printSomething() {
 		return "Whatever..";
 	}
-	
+
+	@RequestMapping(value = "/cs480/user/johne", method = RequestMethod.GET)
+	String printSomethingByJohn() {
+		return "Hello confusion.";
+	}
+
 	/**
 	 * This API lists all the users in the current database.
 	 *
